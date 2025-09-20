ed47661bf5f176436214489a956fc0aa022523b1
Keith Norton <keithnorton.public@proton.me>
2025-09-20 20:05:55 -0300

    Add main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,12 @@
+// This file is a place holder for the future code.
+
 #include <ncurses.h>
 
 int main()
 {
    initscr();                       // Start curses
-   printw("Hello, PatriciaSQL!");   // Print hello
+   move(10, 10);                    // Set the print location
+   addstr("Hello, PatriciaSQL!");   // Print hello
    refresh();                       // Print it onto the screen
    getch();                         // Wait for user input
    endwin();                        // End curses
